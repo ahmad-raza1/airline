1eedbd6bb2da672ffc5dd6f2ea4c437c3d6a2f9f
Ahmad Raza <ahmadraza_@outlook.com>
2019-09-24 18:15:30 +0500

    Added a new route /<int:flight_id> to get information about an individual flight

--- flights/views.py
+++ flights/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from .models import Airport, Flight
 
 # Create your views here.
@@ -14,3 +14,14 @@ def index(request):
 		"flights": Flight.objects.all()
 	}
 	return render(request, "flights/index.html", context)
+
+def flight(request, flight_id):
+	try:
+		flight = Flight.objects.get(pk=flight_id)
+	except Flight.DoesNotExist:
+		raise Http404("Flight does not exist!")
+
+	context = {
+		"flight": flight
+	}
+	return render(request, "flights/flight.html", context)
