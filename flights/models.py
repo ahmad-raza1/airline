5167ae07830f37e6373618d81db88034e9dde989
Ahmad Raza <ahmadraza_@outlook.com>
2019-09-24 17:53:37 +0500

    Updated models.py file

--- flights/models.py
+++ flights/models.py
@@ -16,4 +16,4 @@ class Flight(models.Model):
 	duration = models.IntegerField()
 
 	def __str__(self):
-		return f"{self.id} - {self.origin} to {self.destination}"
+		return f"{self.id} - {self.origin} to {self.destination} in {self.duration} minutes."
