62cb81f59a9035d6255c2fa16d41313d9f2fc7aa
Ahmad Raza <ahmadraza_@outlook.com>
2019-09-24 16:59:58 +0500

    Added an index function to display all the airports

--- flights/views.py
+++ flights/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Airport, Flight
 
 # Create your views here.
 
+"""
 def Hello(request):
 	return HttpResponse("Hello, world!")
+"""
+
+def index(request):
+	context = {
+		"airports": Airport.objects.all()
+	}
+	return render(request, "flights/index.html", context)
