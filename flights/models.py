b60771b9deee431100dd2851d7dfe47feea0506c
Ahmad Raza <ahmadraza_@outlook.com>
2019-09-24 13:43:50 +0500

    Created a model Flight

--- flights/models.py
+++ flights/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Flight(models.Model):
+	origin = models.CharField(max_length=25)
+	destination = models.CharField(max_length=25)
+	duration = models.IntegerField()
